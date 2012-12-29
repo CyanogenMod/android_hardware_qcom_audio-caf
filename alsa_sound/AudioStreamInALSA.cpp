92abe66ff64a428bd30641f5957110b62cb2e42e
Ravishankar Sarawadi <crsaraw@codeaurora.org>
2012-12-28 18:45:52 -0800

    alsa_sound: Subsystem Restart Changes
     - Add new member variable to indicate Subsystem Restart(SSR)
       in HAL
     - For PCM playback/capture SSR usecase, wait for WRITE/READ to
       return EIO error and then re-open PCM handle as part of recovery.
     - For Tunnel mode playback SSR usecase, EOS is sent to Media
       player on SSR which in turn calls destructor for ongoing tunnel
       session.
     - Modify device switch to handle SSR case. Device switch is called
       for same device as previous for SSR case to re-apply AMIX
       controls. Reason for this is codec reset called as part ADSP SSR
       handling which invalidates AMIX controls for current usecase.
    
    Change-Id: I9e1727c481c57d0218b3fb3cf2fbfb42952203e7

--- alsa_sound/AudioStreamInALSA.cpp
+++ alsa_sound/AudioStreamInALSA.cpp
@@ -1,7 +1,7 @@
 /* AudioStreamInALSA.cpp
  **
  ** Copyright 2008-2009 Wind River Systems
- ** Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ ** Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
  **
  ** Licensed under the Apache License, Version 2.0 (the "License");
  ** you may not use this file except in compliance with the License.
@@ -495,7 +495,14 @@ ssize_t AudioStreamInALSA::read(void *buffer, ssize_t bytes)
                         mHandle->module->startVoipCall(mHandle);
                     }
                     else
+                    {
+                        if (mParent->mALSADevice->mSSRComplete) {
+                            ALOGD("SSR Case: Call device switch to apply AMIX controls.");
+                            mHandle->module->route(mHandle, mDevices , mParent->mode());
+                            mParent->mALSADevice->mSSRComplete = false;
+                        }
                         mHandle->module->open(mHandle);
+                    }
                     if(mHandle->handle == NULL) {
                        ALOGE("read:: PCM device re-open failed");
                        mParent->mLock.unlock();
