a5a6a8038667f4ec3747c81c43c1ba910dc6eaef
Haynes Mathew George <hgeorge@codeaurora.org>
2013-01-02 21:20:16 -0800

    alsa_sound: Collection of fixes in HAL for EOS
    -Handling of EOS, and triggering EOS was wrong
    in TunnelPlayer. Seeking when EOS was posted
    to the HAL was wrong. EOS should
    not be posted till seek is complete
    -Also, EOS should not be posted to
    the app if we are seeking
    -Player should wake up when seeked,
    even after EOS was posted from player
    to HAL
    -Fixed this issue by cleaning up the code
    
    Change-Id: Ia5dc43e0482bb7c714a7269574b5eed34efd6637
    CRs-Fixed: 433346
    CRs-Fixed: 432233
    CRs-Fixed: 429868

--- alsa_sound/AudioHardwareALSA.h
+++ alsa_sound/AudioHardwareALSA.h
@@ -523,6 +523,7 @@ private:
     uint32_t            mStreamVol;
 
     bool                mPaused;
+    bool                mSkipEOS;
     bool                mSeeking;
     bool                mReachedEOS;
     bool                mSkipWrite;
