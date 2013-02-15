f536afddfce173683af1336c6a33f5d373a1b692
Vidyakumar Athota <vathota@codeaurora.org>
2013-02-14 18:36:19 -0800

    alsa_sound: Fix for Audio routing to HDMI when BT connected
    Currently when BT is connected Audio routes to HDMI when
    HDMI is plugged in and expected behaviour is audio should
    route to BT only as BT has higher priority than HDMI.This is
    because route function is being called with HDMI device in
    openOutputStream. Removing the route call will fix the issue.
    
    Change-Id: I76933b7f19498023ad2739e6b0305d36a2c77bb6
    CRs-Fixed: 451268

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -1199,22 +1199,8 @@ AudioHardwareALSA::openOutputStream(uint32_t devices,
         mDeviceList.push_back(alsa_handle);
         ALSAHandleList::iterator it = mDeviceList.end();
         it--;
-        ALOGD("it->useCase %s", it->useCase);
-        mALSADevice->route(&(*it), devices, mode());
-        if(!strcmp(it->useCase, SND_USE_CASE_VERB_HIFI2)) {
-            snd_use_case_set(mUcMgr, "_verb", SND_USE_CASE_VERB_HIFI2 );
-        } else {
-            snd_use_case_set(mUcMgr, "_enamod", SND_USE_CASE_MOD_PLAY_MUSIC2);
-        }
-        ALOGD("channels: %d", AudioSystem::popCount(*channels));
-        err = mALSADevice->open(&(*it));
-
-        if (err) {
-            ALOGE("Device open failed err:%d",err);
-        } else {
-            out = new AudioStreamOutALSA(this, &(*it));
-            err = out->set(format, channels, sampleRate, devices);
-        }
+        out = new AudioStreamOutALSA(this, &(*it));
+        err = out->set(format, channels, sampleRate, devices);
         if (status) *status = err;
         return out;
     } else {
