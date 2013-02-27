3093973cd81443b52560bcc074c55aa472f01f84
Vidyakumar Athota <vathota@codeaurora.org>
2013-02-26 16:44:59 -0800

    alsa_sound: Add support for VoLTE over USB headset
    -Currently VoLTE calls are not routed to USB HEADSET
    -Start USB audio thread when VoLTE call is active
    
    Change-Id: I87615f5e0b6373e8364db99430fe887f4a3f23ee

--- alsa_sound/AudioHardwareALSA.cpp
+++ alsa_sound/AudioHardwareALSA.cpp
@@ -902,7 +902,9 @@ status_t AudioHardwareALSA::doRouting(int device)
                                     musbPlaybackState |= USBPLAYBACKBIT_TUNNEL;
                                     break;
                          } else if((!strcmp(it->useCase, SND_USE_CASE_VERB_VOICECALL)) ||
-                                   (!strcmp(it->useCase, SND_USE_CASE_MOD_PLAY_VOICE))) {
+                                   (!strcmp(it->useCase, SND_USE_CASE_MOD_PLAY_VOICE)) ||
+                                   (!strcmp(it->useCase, SND_USE_CASE_VERB_VOLTE)) ||
+                                   (!strcmp(it->useCase, SND_USE_CASE_MOD_PLAY_VOLTE))) {
                                     ALOGV("doRouting: VOICE device switch to proxy");
                                     startUsbRecordingIfNotStarted();
                                     startUsbPlaybackIfNotStarted();
