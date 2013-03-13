3c0e0c367d9fcab27ed0877c5a7cc2e1dc42d1bc
detule <ogjoneski@gmail.com>
2013-03-12 20:51:36 -0400

    audio-caf: allow targets to toggle QCOM_COMPRESSED_AUDIO_ENABLED flag
    Flag forces libalsa to pull compressed audio defines from kernel headers
    (in lieu of libalsa-intf/alsa_audio.h)
    Change-Id: Ie52a0fc4b512beacca9e5bdd3aeb49bf356f1490

--- Android.mk
+++ Android.mk
@@ -3,6 +3,10 @@ ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
 
 AUDIO_HW_ROOT := $(call my-dir)
 
+ifeq ($(TARGET_USES_QCOM_COMPRESSED_AUDIO),true)
+    common_flags += -DQCOM_COMPRESSED_AUDIO_ENABLED
+endif
+
 ifeq ($(strip $(BOARD_USES_ALSA_AUDIO)),true)
     include $(AUDIO_HW_ROOT)/alsa_sound/Android.mk
     include $(AUDIO_HW_ROOT)/libalsa-intf/Android.mk
