8fadd478ab1668645e6b5e8922338a7024a9eefd
Steve Kondik <shade@chemlab.org>
2013-04-15 23:55:12 -0700

    audio: Dynamic library loading, compilation fixes.
    Squashed commit of the following:
    
      commit 45b2a0b5e2344ead1413d627875bf10f79e15163
      Author: Steve Kondik <shade@chemlab.org>
      Date:   Sun Dec 23 04:50:52 2012 -0800
    
          audio: Dynamic library loading, compilation fixes.
    
          Change-Id: I04d5e7b96611572a7758c610ebdf0fd00e8346aa
    
      commit 2c78505b30f2362295e4a995039abf2c650f0d7f
      Author: Mike Kasick <mike@kasick.org>
      Date:   Tue Jan 8 10:31:51 2013 -0500
    
          alsa_sound: Add missing assignment of acdb_handle, fixes in-call volume
    
          Assigns AudioHardwareALSA::mUcMgr->acdb_handle to mAcdbHandle, which is
          missing in the port of dynamic library loading from the AOSP/mako HAL
          (I04d5e7b96611572a7758c610ebdf0fd00e8346aa).
    
          Restores acdb_send_voice_cal and acdb_send_audio_cal calls, which among
          other things, are required for in-call volume adjustment to function.
    
    Change-Id: I5b991a0be410af5eaff133625b63504545d7aa86

--- libalsa-intf/alsa_ucm.c
+++ libalsa-intf/alsa_ucm.c
@@ -50,6 +50,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <dlfcn.h>
 #include <pthread.h>
 #include <ctype.h>
 #include <sys/stat.h>
@@ -61,9 +62,9 @@
 
 #include <linux/ioctl.h>
 #include "msm8960_use_cases.h"
-    #define acdb_loader_send_voice_cal(rxacdb_id, txacdb_id) (-EPERM)
-    #define acdb_loader_send_audio_cal(acdb_id, capability) (-EPERM)
-    #define acdb_loader_send_anc_cal(acdb_id) (-EPERM)
+    static void (*acdb_send_voice_cal)(int, int);
+    static void (*acdb_send_audio_cal)(int, int);
+    static void (*acdb_send_anc_cal)(int);
 #define PARSE_DEBUG 0
 
 /**
@@ -732,8 +733,15 @@ int use_case_index)
                           (!strncmp(current_mod, SND_USE_CASE_MOD_PLAY_VOIP,
                            strlen(SND_USE_CASE_MOD_PLAY_VOIP)))) ||
                           (!uc_mgr->isFusion3Platform))
-                           acdb_loader_send_voice_cal(uc_mgr->current_rx_device,
-                                                    uc_mgr->current_tx_device);
+                        if (uc_mgr->acdb_handle) {
+                            acdb_send_voice_cal = dlsym(uc_mgr->acdb_handle,"acdb_loader_send_voice_cal");
+                            if (acdb_send_voice_cal == NULL) {
+                                ALOGE("Voice acdb: dlsym: Error:%s Loading acdb_loader_send_voice_cal", dlerror());
+                            } else {
+                                acdb_send_voice_cal(uc_mgr->current_rx_device,
+                                        uc_mgr->current_tx_device);
+                            }
+                        }
                 } else {
                     ALOGV("Voice acdb: Required acdb already pushed \
                          rx id %d tx id %d", uc_mgr->current_rx_device,
@@ -856,9 +864,15 @@ const char *use_case, int enable, int ctrl_list_type, int uc_index)
                     ALOGD("acdb_id %d cap %d enable %d",
                                         ctrl_list[uc_index].acdb_id,
                             ctrl_list[uc_index].capability, enable);
-                    acdb_loader_send_audio_cal(
-                            ctrl_list[uc_index].acdb_id,
-                            ctrl_list[uc_index].capability);
+                    if (uc_mgr->acdb_handle) {
+                        acdb_send_audio_cal = dlsym(uc_mgr->acdb_handle,"acdb_loader_send_audio_cal");
+                        if (acdb_send_audio_cal == NULL) {
+                            ALOGE("dlsym: Error:%s Loading acdb_loader_send_audio_cal", dlerror());
+                        } else {
+                            acdb_send_audio_cal(ctrl_list[uc_index].acdb_id,
+                                    ctrl_list[uc_index].capability);
+                        }
+                    }
                 }
             }
             if (enable) {
