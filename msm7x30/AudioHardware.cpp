223cef21f3a47374acfdd08d10f0905e804078ff
Ethan Chen <intervigil@gmail.com>
2013-04-21 00:58:16 +0200

    audio/msm7x30: set mRecordState in AudioInputStreamMSM72xx::set
    This fixes audio input/recording, including voice dialer and
    voice search.
    
    Change-Id: Ica042715ffc7ee664507951f6f7591b23532c34c

--- msm7x30/AudioHardware.cpp
+++ msm7x30/AudioHardware.cpp
@@ -3660,6 +3660,7 @@ status_t AudioHardware::AudioStreamInMSM72xx::set(
     }
     //mHardware->setMicMute_nosync(false);
     mState = AUDIO_INPUT_OPENED;
+    mHardware->set_mRecordState(true);
 
     if (!acoustic)
         return NO_ERROR;
