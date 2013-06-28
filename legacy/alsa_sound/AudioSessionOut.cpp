42f296693080d692bc636fda34716df87a644a75
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2013-06-28 11:40:04 -0400

    alsa_sound: Update the usecase before enabling device
    When speaker device is selected, route function enables
    the TX processing path for speaker protection.
    Change fixes issue where opendevice and route function try to
    enable verbs for ucm usecase set function.As per UCM specification
    only one verb should be active any time.
    
    Change-Id: I9619ece0be9e1e2823321d341363470b1812d8eb

--- legacy/alsa_sound/AudioSessionOut.cpp
+++ legacy/alsa_sound/AudioSessionOut.cpp
@@ -905,12 +905,13 @@ status_t AudioSessionOutALSA::openDevice(char *useCase, bool bIsUseCase, int dev
     alsa_handle.session     = this;
     strlcpy(alsa_handle.useCase, useCase, sizeof(alsa_handle.useCase));
 
-    mAlsaDevice->route(&alsa_handle, devices, mParent->mode());
     if (bIsUseCase) {
-        snd_use_case_set(mUcMgr, "_verb", useCase);
+       snd_use_case_set(mUcMgr, "_verb", useCase);
     } else {
-        snd_use_case_set(mUcMgr, "_enamod", useCase);
+       snd_use_case_set(mUcMgr, "_enamod", useCase);
     }
+    mAlsaDevice->route(&alsa_handle, devices, mParent->mode());
+
 #ifdef QCOM_USBAUDIO_ENABLED
     //Set Tunnel or LPA bit if the playback over usb is tunnel or Lpa
     if((devices & AudioSystem::DEVICE_OUT_ANLG_DOCK_HEADSET)||
