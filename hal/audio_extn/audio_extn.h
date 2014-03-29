6f45dc59c953c65a406f0520673161d611c8f7e2
Ethan Chen <intervigil@gmail.com>
2014-03-29 16:35:33 -0700

    hal: Update audio_extn_usb_stop_capture signature
    Change-Id: I54ef1623ad9aaff87c71a5a86d69faf43af22283

--- hal/audio_extn/audio_extn.h
+++ hal/audio_extn/audio_extn.h
@@ -57,7 +57,7 @@ int32_t audio_extn_get_afe_proxy_channel_count();
 #define audio_extn_usb_start_playback(adev)              (0)
 #define audio_extn_usb_stop_playback()                   (0)
 #define audio_extn_usb_start_capture(adev)               (0)
-#define audio_extn_usb_stop_capture()                    (0)
+#define audio_extn_usb_stop_capture(adev)                (0)
 #define audio_extn_usb_set_proxy_sound_card(sndcard_idx) (0)
 #define audio_extn_usb_is_proxy_inuse()                  (0)
 #else
@@ -66,7 +66,7 @@ void audio_extn_usb_deinit();
 void audio_extn_usb_start_playback(void *adev);
 void audio_extn_usb_stop_playback();
 void audio_extn_usb_start_capture(void *adev);
-void audio_extn_usb_stop_capture();
+void audio_extn_usb_stop_capture(void *adev);
 void audio_extn_usb_set_proxy_sound_card(uint32_t sndcard_idx);
 bool audio_extn_usb_is_proxy_inuse();
 #endif
